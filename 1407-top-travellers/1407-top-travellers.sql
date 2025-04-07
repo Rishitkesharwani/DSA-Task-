ed7b90043e399ec6197e3c611fa8669c1d1ab960
Rishit Kesharwani <113695499+Rishitkesharwani@users.noreply.github.com>
2025-04-07 19:45:00 +0530

    Time: 1063 ms (74.86%), Space: 0B (100%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,7 @@
+SELECT Users.name, ifnull(SUM(distance),0) AS travelled_distance
+FROM Users
+LEFT JOIN Rides ON Users.id = Rides.user_id
+GROUP BY Users.id
+ORDER BY 
+  travelled_distance DESC,
+  Users.name;
